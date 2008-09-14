dcd1c7f597f8ed45eee592ff3fca47c8923762f1
Thomas Weibel <thom@beeblebrox.net>
2008-09-14 15:15:38 +0200

    Added generator skeleton

---
+++ generators/engine_plugin/engine_plugin_generator.rb
@@ -0,0 +1,10 @@
+require 'rails_generator'
+require 'rails_generator/commands'
+
+class EnginePluginGenerator < Rails::Generator::NamedBase
+  def manifest
+    record do |m|
+      # TODO
+    end
+  end
+end
