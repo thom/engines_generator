5fb0b3ee3f7949a6562f194a0a25de837d211973
Thomas Weibel <thom@beeblebrox.net>
2008-09-14 15:27:18 +0200

    Added generator and templates

--- generators/engine_plugin/engine_plugin_generator.rb
+++ generators/engine_plugin/engine_plugin_generator.rb
@@ -4,7 +4,33 @@ require 'rails_generator/commands'
 class EnginePluginGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
-      # TODO
+      m.directory "vendor/plugins/#{file_name}"
+
+      m.directory "vendor/plugins/#{file_name}/app"
+      m.directory "vendor/plugins/#{file_name}/app/controllers"
+      m.directory "vendor/plugins/#{file_name}/app/helpers"
+      m.directory "vendor/plugins/#{file_name}/app/models"
+      m.directory "vendor/plugins/#{file_name}/app/views"
+
+      m.template "routes.rb", "vendor/plugins/#{file_name}/routes.rb"
+      #m.map_route_from_plugin
+
+      m.directory "vendor/plugins/#{file_name}/db"
+      m.directory "vendor/plugins/#{file_name}/db/migrate"
+      m.directory "vendor/plugins/#{file_name}/db/fixtures"
+
+      m.directory "vendor/plugins/#{file_name}/lib"
+
+      m.directory "vendor/plugins/#{file_name}/spec"
+      m.directory "vendor/plugins/#{file_name}/spec/controllers"
+      m.directory "vendor/plugins/#{file_name}/spec/fixtures"
+      m.directory "vendor/plugins/#{file_name}/spec/models"
+      m.directory "vendor/plugins/#{file_name}/spec/views"
+      m.file "spec_helper.rb", "vendor/plugins/#{file_name}/spec/spec_helper.rb"
+
+      m.directory "vendor/plugins/#{file_name}/tasks"
+
+      m.file "empty_file", "vendor/plugins/#{file_name}/init.rb"
     end
   end
 end
