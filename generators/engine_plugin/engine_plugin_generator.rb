e1ce9a33b52840e463564ea70368fb05ee4cd4ac
Thomas Weibel <thom@beeblebrox.net>
2008-09-14 16:27:20 +0200

    Add 'map.from_plugin(:#{plugin_name})' to 'config/routes.rb'

--- generators/engine_plugin/engine_plugin_generator.rb
+++ generators/engine_plugin/engine_plugin_generator.rb
@@ -10,10 +10,10 @@ class EnginePluginGenerator < Rails::Generator::NamedBase
       m.directory "vendor/plugins/#{file_name}/app/controllers"
       m.directory "vendor/plugins/#{file_name}/app/helpers"
       m.directory "vendor/plugins/#{file_name}/app/models"
-      m.directory "vendor/plugins/#{file_name}/app/views"
+      #m.directory "vendor/plugins/#{file_name}/app/views"
 
       m.template "routes.rb", "vendor/plugins/#{file_name}/routes.rb"
-      #m.map_route_from_plugin
+      m.map_route_from_plugin
 
       m.directory "vendor/plugins/#{file_name}/db"
       m.directory "vendor/plugins/#{file_name}/db/migrate"
@@ -34,3 +34,38 @@ class EnginePluginGenerator < Rails::Generator::NamedBase
     end
   end
 end
+
+module Engine #:nodoc:
+  module Generator #:nodoc:
+    module Commands #:nodoc:
+
+      module Create
+        def map_route_from_plugin
+          logger.route "adding map.from_plugin(:#{file_name}) to top of routes.rb"
+          sentinel = 'ActionController::Routing::Routes.draw do |map|'
+          gsub_file('config/routes.rb', /(#{Regexp.escape(sentinel)})/mi) do |match|
+            "#{match}\n  map.from_plugin(:#{file_name})\n"
+          end
+        end
+      end
+
+      module Destroy
+        def map_route_from_plugin
+          look_for = "\n  map.from_plugin(:#{file_name})\n"
+          logger.route "removing map.from_plugin(:#{file_name}) from routes.rb"
+          gsub_file 'config/routes.rb', /(#{Regexp.escape(look_for)})/mi, ''
+        end
+      end
+
+      module List
+        def map_route_from_plugin
+          logger.route "adding map.from_plugin(:#{file_name}) to top of routes.rb"
+        end
+      end
+    end
+  end
+end
+
+Rails::Generator::Commands::Create.send   :include,  Engine::Generator::Commands::Create
+Rails::Generator::Commands::Destroy.send  :include,  Engine::Generator::Commands::Destroy
+Rails::Generator::Commands::List.send     :include,  Engine::Generator::Commands::List
